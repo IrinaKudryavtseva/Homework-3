ca76201ee5b25d5ef6da80e616b21d6ad01e895a
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-11-04 10:40:33 +0500

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+for(int i = 0; i < array.Length; i++)
+{
+ array[i] = new Random().Next (1, 100);
+ Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int count = 0;
+foreach (int number in array)
+{
+    if (number % 2 == 0)
+    count++;
+}
+
+Console.WriteLine("Количество четных чисел: " + count);
+
+
+    
